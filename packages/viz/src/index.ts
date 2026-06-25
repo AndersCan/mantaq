ee7c056c9a8eaf88d65bc00148816a5f1c3541ac
AndersCan <anders.can1@gmail.com>
2026-06-26 00:48:25 +0200

    improve: collapse mantaq-viz props + split renderAll

--- packages/viz/src/index.ts
+++ packages/viz/src/index.ts
@@ -1,3 +1,5 @@
 import "./components/mantaq-viz.ts";
 import "./components/context-viewer.ts";
 import "./components/transition-timeline.ts";
+
+export type { LayoutConfig, ViewConfig } from "./components/mantaq-viz.ts";
